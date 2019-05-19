0b9cd12ef658678be2e189388a9c560494def190
TomGut <t-gutowski@o2.pl>
2019-05-19 09:26:51 +0200

    CartService, impl and CartRestController - refactored and functional

--- src/main/java/pl/codementors/finalProject/controller/CartRestController.java
+++ src/main/java/pl/codementors/finalProject/controller/CartRestController.java
@@ -10,68 +10,42 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.codementors.finalProject.models.Cart;
-import pl.codementors.finalProject.models.Product;
-import pl.codementors.finalProject.repo.CartRepository;
-import pl.codementors.finalProject.repo.ProductRepository;
+import pl.codementors.finalProject.services.CartServiceImpl;
 
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/cart/")
 public class CartRestController {
 
-    //TODO move to service
-    private Cart cart = new Cart();
-    private Product product = new Product();
-
     @Autowired
-    private CartRepository cartRepository;
-
-    @Autowired
-    private ProductRepository productRepository;
+    private CartServiceImpl cartService;
 
     @GetMapping(value = "list")
-    public List<Cart> getCart(){
-        return cartRepository.findAll();
+    public List<Cart> getCarts(){
+        return cartService.getCarts();
     }
 
     @PostMapping(value = "add")
     public Cart createCart(){
-        Cart newCart = new Cart();
-        cartRepository.save(newCart);
-        return newCart;
+        return cartService.addCart();
     }
 
     @DeleteMapping(value = "delete/{cartId}")
-    public Cart deleteCart(@PathVariable("cartId") Long cartId){
-        cart = cartRepository.findOne(cartId);
-        cartRepository.delete(cart);
-        return cart;
+    public void deleteCart(@PathVariable("cartId") Long cartId){
+        cartService.deleteCart(cartId);
     }
 
     @PutMapping(value = "{cartId}/deleteProduct/{productId}")
     public Cart deleteFromCart(@PathVariable("cartId") Long cartId,
                                @PathVariable("productId") Long productId) {
-        cart = cartRepository.findOne(cartId);
-        product = productRepository.findOne(productId);
-        cart.removeProduct(product);
-        product.setCart(null);
-        cartRepository.save(cart);
-        return cart;
+        return cartService.deleFromCart(cartId, productId);
     }
 
     @PutMapping(value = "{cartId}/addProduct/{productId}")
     public Cart addProduct(@PathVariable("cartId") Long cartId,
                            @PathVariable("productId") Long productId){
-        cart = cartRepository.findOne(cartId);
-        product = productRepository.findOne(productId);
-        cart.addProduct(product);
-        product.setCart(cart);
-        cartRepository.save(cart);
-        return cart;
+        return cartService.addProductToCart(cartId, productId);
     }
 }
--- src/main/java/pl/codementors/finalProject/services/CartServiceImpl.java
+++ src/main/java/pl/codementors/finalProject/services/CartServiceImpl.java
@@ -1,21 +1,57 @@
 package pl.codementors.finalProject.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pl.codementors.finalProject.models.Cart;
 import pl.codementors.finalProject.models.Product;
+import pl.codementors.finalProject.repo.CartRepository;
+import pl.codementors.finalProject.repo.ProductRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CartServiceImpl implements CartService {
 
-    private Cart cart;
-    private List<Product> products = new ArrayList<>();
+    @Autowired
+    private CartRepository cartRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
-    public List<Product> addProduct(Product product) {
-        products = cart.getProducts();
-        products.add(product);
-        cart.setProducts(products);
-        return products;
+    public List<Cart> getCarts() {
+        return cartRepository.findAll();
+    }
+
+    @Override
+    public Cart addCart() {
+        Cart cart = new Cart();
+        cartRepository.save(cart);
+        return cart;
+    }
+
+    @Override
+    public void deleteCart(Long cartId) {
+        Cart cart = cartRepository.findOne(cartId);
+        cartRepository.delete(cart);
+    }
+
+    @Override
+    public Cart addProductToCart(Long cartId, Long productId) {
+        Cart cart = cartRepository.findOne(cartId);
+        Product product = productRepository.findOne(productId);
+        cart.addProduct(product);
+        product.setCart(cart);
+        cartRepository.save(cart);
+        return cart;
+    }
+
+    @Override
+    public Cart deleFromCart(Long cartId, Long productId) {
+        Cart cart = cartRepository.findOne(cartId);
+        Product product = productRepository.findOne(productId);
+        cart.removeProduct(product);
+        product.setCart(null);
+        cartRepository.save(cart);
+        return cart;
     }
 }
