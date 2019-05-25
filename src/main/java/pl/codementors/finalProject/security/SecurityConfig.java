1ab85f42caffe316b9ce69a3cb3566db1fead7c3
TomGut <t-gutowski@o2.pl>
2019-05-25 20:06:55 +0200

    some code refactor in security

--- src/main/java/pl/codementors/finalProject/security/SecurityConfig.java
+++ src/main/java/pl/codementors/finalProject/security/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,6 +23,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      @Autowired
      private LocalUserDetailsService localUserDetailsService;
 
+
+     @Autowired
+     private Environment env;
+
+     @Autowired
+     public void configureGlobal(AuthenticationManagerBuilder auth) {
+          auth.authenticationProvider(authenticationProvider(passwordEncoder()));
+     }
+
      @Override
      protected void configure(HttpSecurity http) throws Exception {
 
@@ -32,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           http
                   .authorizeRequests()
                   .antMatchers("/", "/users**").hasRole(LocalUserRole.ADMIN.name())
-                  .antMatchers("/products", "/login", "/users/add").permitAll()
+                  .antMatchers("/products**", "/login", "/users/add").permitAll()
                   .antMatchers("/cart**").hasRole(LocalUserRole.USER.name())
                   .anyRequest()
                   .authenticated()
@@ -40,11 +48,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                   .httpBasic();
      }
 
-     @Autowired
-     private Environment env;
-
      @Bean
-     CorsConfigurationSource corsConfigurationSource() {
+     public CorsConfigurationSource corsConfigurationSource() {
           UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
           CorsConfiguration config = new CorsConfiguration();
           config.setAllowCredentials(true);
@@ -53,11 +58,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           config.addAllowedMethod("*");
           source.registerCorsConfiguration("/**", config);
           return source;
-     }
-
-     @Autowired
-     public void configureGlobal(AuthenticationManagerBuilder auth) {
-          auth.authenticationProvider(authenticationProvider(passwordEncoder()));
      }
 
      @Bean
